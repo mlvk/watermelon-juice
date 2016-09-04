5499735a336be9dfa58495ec0f5a1725d753f16e
Aram Zadikian <brancusi@gmail.com>
2016-09-03 20:49:43 -0700

    cleanup (#14)

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -6,6 +6,9 @@ window.deprecationWorkflow.config = {
     { handler: "silence", matchId: "ember-load-initializers.legacy-shims" },
     { handler: "silence", matchId: "ember-application.injected-container" },
     { handler: "silence", matchId: "ember-string-utils.fmt" },
-    { handler: "silence", matchId: "ember-metal.merge" }
+    { handler: "silence", matchId: "ember-metal.merge" },
+    { handler: "silence", matchMessage: "Ember.String.fmt is deprecated, use ES6 template strings instead." },
+    { handler: "silence", matchMessage: "Usage of `Ember.merge` is deprecated, use `Ember.assign` instead." },
+    { handler: "silence", matchId: "ember-views.dispatching-modify-property" }
   ]
 };
