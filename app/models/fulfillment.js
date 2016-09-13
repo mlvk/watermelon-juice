1d35d53978815ab6de173abeb9aa49554131b649
Vinh Luu <vinhlt3@gmail.com>
2016-09-13 17:11:56 +0700

    disable review and sign when not complete tracking inventory

--- app/models/fulfillment.js
+++ app/models/fulfillment.js
@@ -1,21 +1,46 @@
-import Ember from 'ember';
-import Model from 'ember-data/model';
-import attr from 'ember-data/attr';
-import { belongsTo } from 'ember-data/relationships';
+import Ember from "ember";
+import Model from "ember-data/model";
+import attr from "ember-data/attr";
+import { belongsTo } from "ember-data/relationships";
 
 const { equal, not } = Ember.computed;
 
 export default Model.extend({
-  deliveryState:      attr('string'),
-  notificationState:  attr('string'),
-  submittedAt:        attr('date'),
+  deliveryState:      attr("string"),
+  notificationState:  attr("string"),
+  submittedAt:        attr("date"),
 
-  routeVisit:         belongsTo('route-visit'),
-  order:              belongsTo('order'),
-  stock:              belongsTo('stock'),
-  creditNote:         belongsTo('credit-note'),
-  pod:                belongsTo('pod'),
+  routeVisit:         belongsTo("route-visit"),
+  order:              belongsTo("order"),
+  stock:              belongsTo("stock"),
+  creditNote:         belongsTo("credit-note"),
+  pod:                belongsTo("pod"),
 
-  pending:            equal('deliveryState', 'pending'),
-  fulfilled:          not('pending')
+  pending:            equal("deliveryState", "pending"),
+  fulfilled:          not("pending"),
+
+  prepareStock() {
+    const location = this.get("order.location");
+    const itemDesires = location.get("itemDesires");
+
+    if(this.belongsTo("stock").id() || this.belongsTo("stock").value()) {
+      const stock = this.get("stock");
+      const missingItemDesires = itemDesires
+        .filter(itemDesire => itemDesire.get("enabled"))
+        .filter(itemDesire =>
+          !stock.get("stockLevels")
+            .find(sl => sl.get("item.id") === itemDesire.get("item.id")));
+
+      missingItemDesires
+        .forEach(itemDesire => this.store.createRecord("stock-level", {stock, item:itemDesire.get("item")}));
+
+    } else {
+      const stock = this.store.createRecord("stock", {location});
+      this.set("stock", stock);
+
+      itemDesires
+        .filter(itemDesire => itemDesire.get("enabled"))
+        .forEach(itemDesire => this.store.createRecord("stock-level", {stock, item: itemDesire.get("item")}));
+    }
+  }
 });
