e1b82836f080eb0354d40e6c844134aec86ac3f7
Aram Zadikian <brancusi@gmail.com>
2016-11-09 13:19:25 -0800

    Cleanup numeric field on blur [ch354] (#44)

--- app/components/rows/cost-quantity-row/component.js
+++ app/components/rows/cost-quantity-row/component.js
@@ -1,11 +1,23 @@
 import Ember from "ember";
 import computed from "ember-computed-decorators";
 
+const {
+  set
+} = Ember;
+
 export default Ember.Component.extend({
   classNames: ["row", "stretch", "center"],
 
   @computed("quantity", "unitPrice")
   total(quantity, unitPrice) {
     return quantity * unitPrice;
+  },
+
+  actions: {
+    cleanNumericField(key, e) {
+      const parsed = parseFloat(e.target.value);
+      const cleaned = _.isNaN(parsed) ? 0 : parsed;
+      set(this, key, cleaned);
+    }
   }
 });
